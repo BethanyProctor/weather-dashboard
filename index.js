1d3e4b6b3076321f207dc33b922f660dc0c1e7e4
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-05-06 18:23:41 -0400

    added newid for deletebtn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 //need: date, icon, temp, humidity, wind speed
+const userCityEl = document.getElementById('userCity');
+const submitBtnEl = document.querySelector('.submit-btn')
+const recentList = document.querySelector('.recentCities')
 
 const cityEl = document.getElementById('current-city');
 const currentDateEl = document.getElementById('currentDate');
@@ -8,28 +11,58 @@ const currentTempEl = document.getElementById('current-temp');
 const currentWindEl = document.getElementById('current-windspeed');
 const currentHumidityEl = document.getElementById('current-humidity');
 
+const searchHistory = [];
 
-const APIKey = `2ce87c45b4c5de93bf67b3918c1caa4a`;
+//API info
 let latitude;
 let longitude;
-const queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}&units=imperical`;
+const geoAPIKey = `1a874bf16e155d8774b70ff5fb8c74e2`;
+const APIKey = `2ce87c45b4c5de93bf67b3918c1caa4a`;
 
-function getWeatherData() {
-    navigator.geolocation.getCurrentPosition((success) => {
-        console.log(success);
+const getCityCoords = () => {
+    const cityName = userCityEl.value;
+    if (!cityName) alert ('Enter a City to get Started');
 
-        let (latitude, longitude) = success.coords;
-        fetch(queryURL)
+    const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${geoAPIKey}`;
 
-        .then (res => res.json().then (data =>{
-            console.log(data);
-            showWeatherData(data);
-        }))
+    fetch(geoURL)
+    .then (res => res.json())
+    .then(data => {
+        searchHistory.push(cityName);
+        localStorage.setItem('history', json.stringify(searchHistory))
+        console.log('searchHistory', searchHistory)
+
+        let latitude = data.lat;
+        let longitude = data.lon;
+
+        console.log(`Lat: ${latitude}`);
+        console.log(`Long: ${longitude}`);
     })
+    .catch (() => console.error());
+
+ } 
+
+const getWeatherData = () => {
+    const queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}&units=imperical`;
+    
+    getCityCoords();
+    
+    fetch(queryURL)
+        .then (res => res.json())
+        .then (data => {
+            console.log(data);
+        })
+        .catch (console.error());
+
+    recentList.innerHTML = 
+       `<p>${searchHistory[0]}</p>
+        <p>${searchHistory[1]}</p>
+        <p>${searchHistory[2]}</p>
+        <p>${searchHistory[3]}</p>
+        <p>${searchHistory[4]}</p>`;
 }
 
-getWeatherData();
-
-function showWeatherData(data) {
-    let (humidity, )
-}
+submitBtnEl.addEventListener('click', event => {
+    event.preventDefault();
+    getWeatherData();
+ })
